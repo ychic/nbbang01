0e6b0da0b3893ac084debdb6a9b4c9a0a1f5b19c
kang hyun soo <kanghs1996@gmail.com>
2022-01-07 00:05:42 +0900

    어드민 멤버 리스트 web, service, serviceimpl 에 합침

--- src/main/java/com/kosmo/nbbang/service/MemberService.java
+++ src/main/java/com/kosmo/nbbang/service/MemberService.java
@@ -1,10 +1,11 @@
 package com.kosmo.nbbang.service;
 
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
 public interface MemberService {
 	//로그인 용
 	int isLogin(Map map);
-	
-	
+	//멤버리스트
+	MemberListPaging<MemberDTO> selectAllMember(Map map, HttpServletRequest req, int nowPage);
 }
