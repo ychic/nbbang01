760d8ccc5527ee19b4f5bc63eeb794e1e3545905
ychic <officialyoonsic@gmail.com>
2022-01-18 17:50:05 +0900

    #CallbackRequest controller로 전환

--- src/main/java/com/kosmo/nbbang/vbank/controller/BankingController.java
+++ src/main/java/com/kosmo/nbbang/vbank/controller/BankingController.java
@@ -45,56 +45,6 @@ public class BankingController {
 	
 	
 	/*
-	 * 	Method : callback 이벤트 헨들러 [요청할 필요 X]
-	 *  condition :
-	 *  Scenario : 사용자 전체조회 계좌 요청-> vbankServer 전체 계좌 요청 및 응답 -> 브라우저로 응답[JSON]  - Array 형식 
-	 *  issue > 편의를 위해 Rest가 아닌 Controller로 빼놔야할 수도 있음
-	 */
-	@GetMapping("/callback/url.do")
-	public Map getMyCallback(@RequestParam Map map,HttpSession session) {
-
-		//callback Eventhanlder
-//		Map res  =bankingservice.setAuthToken(map);
-		
-		//Test
-		String result = "{\r\n"
-				+ "    \"access_token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDAxNzE1Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2NTAyNTcyNTIsImp0aSI6IjhjZGRkMjkzLTg4NmMtNGUxZi1hMWM1LTliNDU2Yjc2YjM4OCJ9.eZ1hKj56JMQccYAbRsaFes7VT8GeqLld6s6ndyaZ2_E\",\r\n"
-				+ "    \"token_type\": \"Bearer\",\r\n"
-				+ "    \"refresh_token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDAxNzE1Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2NTExMjEyNTIsImp0aSI6Ijg1YzMxMDNjLTFlYjItNGM0ZC05YzE3LTJhZmE3Y2ZkZDIxNyJ9.5hX1mm4jmr5lhwsf4fJuAcKDN9HfFqvHWUT0fTP5rnM\",\r\n"
-				+ "    \"expires_in\": 7775999,\r\n"
-				+ "    \"scope\": \"inquiry login transfer\",\r\n"
-				+ "    \"user_seq_no\": \"1101001715\"\r\n"
-				+ "}";
-		
-		ObjectMapper mapper = new ObjectMapper();
-		Map res=null;
-		try {
-			res = mapper.readValue(result, Map.class);
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		if(res.get("resp_code").equals(ResponeCode.OK)) {
-			//dao를 통해 등록해야할 것 > 
-//			String email = session.getAttribute(SESSION_UID).toString();
-			//Test
-			String email = "jsik@naver.com";
-			res.put("email", email);
-
-			int affect = innerservice.setAuth(res);
-	
-		}
-		
-		return res;
-	}
-	
-	
-	/*
 	 * 	Method : 전계좌 조회 
 	 *  condition : dao에 등록된 사람만 조회 가능
 	 *  Scenario : 사용자 전체조회 계좌 요청-> vbankServer 전체 계좌 요청 및 응답 -> 브라우저로 응답[JSON]  - Array 형식 
