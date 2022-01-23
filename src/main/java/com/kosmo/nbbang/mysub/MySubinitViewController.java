7591f45b3c7b003cde2333c5c568c857e5603bce
park jae hyun <qkrwogus29@gmail.com>
2022-01-23 20:55:10 +0900

    <mysub페이지> - 폴더명 뿌려주기 - 등록화면 썸네일 뿌려주기 - logo 가로방향 새로 받았는데 jpg라 안예쁨 (교체예정)

--- src/main/java/com/kosmo/nbbang/mysub/MySubinitViewController.java
+++ src/main/java/com/kosmo/nbbang/mysub/MySubinitViewController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.kosmo.nbbang.park.service.impl.FolderNameServiceImpl;
 import com.kosmo.nbbang.park.service.impl.FolderServiceImpl;
 import com.kosmo.nbbang.park.service.impl.ManualServiceImpl;
 import com.kosmo.nbbang.park.service.impl.SecondFolderServiceImpl;
@@ -32,7 +33,10 @@ public class MySubinitViewController {
 	//secondFoldSelectList 넘겨주기
 	@Resource(name = "secondFolderService")
 	private SecondFolderServiceImpl secondFolderService;
-	
+	//폴더이름 selectOne (sfno로)
+	@Resource(name = "folderNameService")
+	private FolderNameServiceImpl folderNameService;
+	//해당계정의 총 구독지출 금액 selectOne 서비스
 	
 	
 	//여기서도 All하드코딩말고 manualService 주입받고 sfno반환하는메소드 써서 개량하면안될까? 01.21
@@ -41,8 +45,21 @@ public class MySubinitViewController {
 							@RequestParam(required = false,defaultValue = "All") Map map,Model model) {
 		//서비스호출
 		//초기화면에 All폴더에 해당하는 구독서비스 뿌려주는 서비스
+		//map.put("sfname", "All");	//다른방법찾기
 		map.put("email", email);
+		
+		List sfnoss=sfnosService.selectList(map);
+		//System.out.println("sfnosService.selectList(map):"+sfnosService.selectList(map));
+		String strSfnoss=sfnoss.get(0).toString(); //"{SFNO=98}"
+		strSfnoss=strSfnoss.replace("{", ""); 
+		strSfnoss=strSfnoss.replace("SFNO=","");
+		strSfnoss=strSfnoss.replace("}", "");// "98"
+		int sfnoInt = Integer.parseInt(strSfnoss);
+		map.put("sfno", sfnoInt); //sfno파라미터 전달완료
+		 	
+		FolderNameDTO folderName=folderNameService.selectOne(map);
 		FolderDTO folder=folderService.selectOne(map);
+		model.addAttribute("folderName",folderName);
 		model.addAttribute("folder",folder);	//리퀘스트영역에 selectOne쿼리 결과 넘겨주기()
 		
 		return "schedule/MySub.tiles";
@@ -63,10 +80,11 @@ public class MySubinitViewController {
 		int sfnoInt = Integer.parseInt(strSfnoss);
 		map.put("sfno", sfnoInt);	//sfno파라미터 전달완료
 		
+		FolderNameDTO folderName=folderNameService.selectOne(map);
 		FolderDTO folder=folderService.selectOne(map);
 		//		FolderDTO folder=new FolderDTO();
 		//folder.setEmail(email);
-		
+		model.addAttribute("folderName",folderName);
 		model.addAttribute("folder",folder);	//리퀘스트영역에 selectOne쿼리 결과 넘겨주기()
 		System.out.println(model.addAttribute("folder",folder));//
 		//
@@ -90,7 +108,41 @@ public class MySubinitViewController {
 		
 		return "schedule/MySub2.tiles";
 	}
-	///thirdFold.do
+	
+	@RequestMapping("/mysub/thirdFolderView.do")
+	public String thirdFoldView(@ModelAttribute(value = "email") String email,
+								@RequestParam Map map, Model model, Model model2) {
+		map.put("email", email);
+		//sfno 파라미터 값 구하기(서비스호출-manualService)
+		
+		List sfnoss=sfnosService.selectList(map);
+		System.out.println("sfnosService.selectList(map):"+sfnosService.selectList(map));
+		String strSfnoss=sfnoss.get(2).toString();	//"{SFNO=98}"
+		strSfnoss=strSfnoss.replace("{", "");
+		strSfnoss=strSfnoss.replace("SFNO=", "");
+		strSfnoss=strSfnoss.replace("}", "");	// "98"
+		int sfnoInt = Integer.parseInt(strSfnoss);
+		map.put("sfno", sfnoInt);	//sfno파라미터 전달완료
+		
+		FolderNameDTO folderName=folderNameService.selectOne(map);
+		FolderDTO folder=folderService.selectOne(map);
+		//		FolderDTO folder=new FolderDTO();
+		//folder.setEmail(email);
+		model.addAttribute("folderName",folderName);
+		model.addAttribute("folder",folder);	//리퀘스트영역에 selectOne쿼리 결과 넘겨주기()
+		System.out.println(model.addAttribute("folder",folder));//
+		//
+		// 해당계정의 WATCH(두번째폴더)에 해당하는 레코드(등록한 구독 서비스 목록) 읽어오기
+		List<SecondFolderDTO> secondRecord=secondFolderService.secondFoldSelectList(map);
+		//secondRecord.get(1);	//date타입 변경
+		
+		
+		model2.addAttribute("secondRecord",secondRecord);
+		
+		System.out.println(secondFolderService.secondFoldSelectList(map));
+		
+		return "schedule/MySub3.tiles";
+	}
 	
 	
 	
