e62b096dc715bcad8179a0e08f8f1a97f2f9c70d
kang hyun soo <kanghs1996@gmail.com>
2022-02-06 03:09:38 +0900

    chat ui

--- src/main/java/com/kosmo/nbbang/partykang/PartyChatController.java
+++ src/main/java/com/kosmo/nbbang/partykang/PartyChatController.java
@@ -1,5 +1,7 @@
 package com.kosmo.nbbang.partykang;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,6 +9,7 @@ import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -22,7 +25,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
 import com.kosmo.nbbang.partybbs.service.PartyBbsDTO;
 import com.kosmo.nbbang.partykang.service.MessageDTO;
 import com.kosmo.nbbang.partykang.service.PartyChatDTO;
@@ -85,17 +90,20 @@ public class PartyChatController {
 	
 	@RequestMapping(value = "/message/saveMessage.do", produces = "text/plain; charset=UTF-8")
 	@ResponseBody
-	public String saveMessage(@RequestParam String roomNo, @RequestParam String message,HttpServletRequest req) {
+	public String saveMessage(MessageDTO message,HttpServletRequest req){
 		String path=req.getServletContext().getRealPath("/chatList");		
-		return PartyChatUtil.saveMessage(path, roomNo, message);
+		return PartyChatUtil.saveMessage(path, message.getRoomNo(), message.toString());
 	}
 	
-	@RequestMapping(value = "/message/getData.do", produces = "text/plain; charset=UTF-8")
+	@RequestMapping(value = "/message/getData.do", produces = "json/application; charset=UTF-8")
 	@ResponseBody
 	public String getData(@ModelAttribute("email") String email, @RequestParam String roomNo, @RequestParam String partyNo, HttpServletRequest req) throws JsonProcessingException {
 		Map map = new HashMap();
 		String path=req.getServletContext().getRealPath("/chatList");		
 		String message = PartyChatUtil.getMessage(path, roomNo);
+		if(!message.equals("fail")) {
+			message = "["+message.substring(0, message.length()-1)+"]";
+		}
 		PartyBbsDTO partyBbs = partyService.getPartyBbs(partyNo);
 		PartyChatDTO dto = partyService.getMyChat(roomNo);
 		String partnerId = email.equals(dto.getParticipant()) ? dto.getBbswriter() : dto.getParticipant();
--- src/main/java/com/kosmo/nbbang/partykang/service/MessageDTO.java
+++ src/main/java/com/kosmo/nbbang/partykang/service/MessageDTO.java
@@ -1,45 +1,71 @@
 package com.kosmo.nbbang.partykang.service;
 
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
+
 /*
 @Getter
 @Setter
 */
 public class MessageDTO {
-	
+
 	private String nickname;
 	private String roomNo;
 	private String message;
+	private String time;
+
 	public String getNickname() {
 		return nickname;
 	}
+
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+
 	public String getRoomNo() {
 		return roomNo;
 	}
+
 	public void setRoomNo(String roomNo) {
 		this.roomNo = roomNo;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	public MessageDTO(String nickname, String roomNo, String message) {
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public MessageDTO(String nickname, String roomNo, String message, String time) {
 		super();
 		this.nickname = nickname;
 		this.roomNo = roomNo;
 		this.message = message;
+		this.time = time;
 	}
+
 	public MessageDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return String.format(
+				"{\"nickname\":\"%s\", \"roomNo\":\"%s\", \"message\":\"%s\", \"time\":\"%s\"}",
+				nickname, roomNo, message, time);
+	}
+
 }
