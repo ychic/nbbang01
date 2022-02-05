30dda8e8f1ad971f9bc19471c02bcb976b58f606
power <power@DESKTOP-QF53MPN>
2022-02-06 00:06:45 +0900

    파티 글쓰기, 내 파티 디자인 수정

--- src/main/java/com/kosmo/nbbang/web/MyPartyController.java
+++ src/main/java/com/kosmo/nbbang/web/MyPartyController.java
@@ -1,10 +1,15 @@
 package com.kosmo.nbbang.web;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,12 +33,25 @@ public class MyPartyController {
 	@RequestMapping("/myParty.do")
 	public String myParty(@ModelAttribute("email") String email, @RequestParam Map map, Model model) {
 		
+
+		
 		List<PartyBbsDTO> partyBbsList = partyBbsService.getList(email);
 		List<PartyBbsDTO> temp = new Vector();
 		for (PartyBbsDTO dto : partyBbsList) {
 			map.put("partyNo", dto.getPartyNo());
 			System.out.println("dto.getPartyNo() : " + dto.getPartyNo());
 			System.out.println(dto.getPartyCategoryName());
+			
+			System.out.println("파티 매치 날짜 : " + dto.getPartyMatchdate());
+			
+			if (dto.getPartyMatchdate() != null) {
+		        Calendar cal = Calendar.getInstance();
+		        cal.setTime(new Date(dto.getPartyMatchdate().getTime()));
+		        cal.add(Calendar.MONTH,6);
+		        java.sql.Date endDate = new java.sql.Date(cal.getTimeInMillis());
+		        dto.setEndDate(endDate);
+		        System.out.println(endDate);
+			}
 			String count = partyBbsService.nowPartyMember(dto.getPartyNo());
 			System.out.println("count : " + count);
 			dto.setCount(count);
@@ -42,5 +60,5 @@ public class MyPartyController {
 		model.addAttribute("partyBbsList", temp);
 		return "schedule/MyParty.tiles";
 	}
-	
+
 } // end MyPartyController
