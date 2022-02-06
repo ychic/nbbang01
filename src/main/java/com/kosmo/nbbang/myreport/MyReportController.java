35781f6a6b99526813ee815928ea3619a24e002f
suyeon kim <suyeun5678@naver.com>
2022-02-06 13:48:22 +0900

    [리포트추가]

--- src/main/java/com/kosmo/nbbang/myreport/MyReportController.java
+++ src/main/java/com/kosmo/nbbang/myreport/MyReportController.java
@@ -21,55 +21,61 @@ import com.kosmo.nbbang.myreport.service.impl.MyReportServiceImpl;
 @SessionAttributes({"email","nickname"})
 @Controller
 public class MyReportController {
-	
-	//주입
-	@Autowired
-	private MyReportServiceImpl myReportService;
+   
+   //주입
+   @Autowired
+   private MyReportServiceImpl myReportService;
 
-	//막대 점차트 MY 리포트
-	@RequestMapping("/myReport.do")
-	public String myReport2(@ModelAttribute("email") String email,@ModelAttribute("nickname") String nickname,Model model, Map map) {
-		System.out.println("테스트11111");
-		List<MyReportDTO> myReportList2 = myReportService.getMyReport2(map);
-		model.addAttribute("myReportList2", myReportList2);
-		return "forward:/myReport2.do";
-	}
-	
-	//그외 MY 리포트
-	@RequestMapping("/myReport2.do")
-	public String myReport(@ModelAttribute("email") String email,@ModelAttribute("nickname") String nickname, Map map, Model model) {
-		System.out.println("테스트22222");
-		System.out.println("map:"+map);
-		List<MyReportDTO> myReportList = myReportService.getMyReport(map);
-		//List<MyReportDTO> myReportList2 = myReportService.getMyReport(map);
-		MyReportDTO yearSum = myReportService.getYearSum(map);
-		MyReportDTO monthSum = myReportService.getMonthSum(map);
-		MyReportDTO weekSum = myReportService.getWeekSum(map);
-		
-		for(MyReportDTO dto : myReportList) {
-			
-			System.out.println("########################");
-			System.out.println(dto.getSubno());
-			System.out.println(dto.getSubservice());
-			System.out.println(dto.getPaymentday());
-			System.out.println(dto.getMoney());
-			System.out.println(dto.getSfno());
-			System.out.println(dto.getPaymentmon());
-			System.out.println("########################");
+   //막대 점차트 MY 리포트
+   @RequestMapping("/myReport.do")
+   public String myReport2(@ModelAttribute("email") String email,@ModelAttribute("nickname") String nickname,Model model, Map map) {
+      System.out.println("테스트11111");
+      List<MyReportDTO> myReportList2 = myReportService.getMyReport2(map);
+      model.addAttribute("myReportList2", myReportList2);
+      return "forward:/myReport2.do";
+   }
+   
+   //그외 MY 리포트
+   @RequestMapping("/myReport2.do")
+   public String myReport(@ModelAttribute("email") String email,@ModelAttribute("nickname") String nickname, Map map, Model model) {
+      System.out.println("테스트22222");
+      System.out.println("map:"+map);
+      System.out.println(map.get("email"));
+      List<MyReportDTO> myReportList = myReportService.getMyReport(map);
+      //List<MyReportDTO> myReportList2 = myReportService.getMyReport(map);
+      MyReportDTO yearSum = myReportService.getYearSum(map);
+      MyReportDTO monthSum = myReportService.getMonthSum(map);
+      MyReportDTO weekSum = myReportService.getWeekSum(map);
+      
+      for(MyReportDTO dto : myReportList) {
+         
+         System.out.println("########################");
+         System.out.println(dto.getSubno());
+         System.out.println(dto.getSubservice());
+         System.out.println(dto.getPaymentday());
+         System.out.println(dto.getMoney());
+         System.out.println(dto.getSfno());
+         System.out.println(dto.getPaymentmon());
+         System.out.println("########################");
 
-		}
-		System.out.println(email);
-		System.out.println(nickname);
-		model.addAttribute("email", email);
-		model.addAttribute("nickname", nickname);
-		model.addAttribute("myReportList", myReportList);
-		
-		model.addAttribute("yearSum", yearSum.getYearSum());
-		model.addAttribute("monthSum", monthSum.getMonthSum());
-		model.addAttribute("weekSum", weekSum.getWeekSum());
-		System.out.println("yearSum"+yearSum);
-		
-		return "schedule/MyReport.tiles";
-		//schedule/MyReport.tiles
-	}
+      }
+      
+      System.out.println(nickname);
+      model.addAttribute("email", email);
+      model.addAttribute("nickname", nickname);
+      model.addAttribute("myReportList", myReportList);
+      
+      //System.out.println("weekSum"+weekSum.getWeeksum());
+      
+      if(yearSum != null)
+         model.addAttribute("yearSum", yearSum.getYearsum());
+      if(monthSum != null)
+         model.addAttribute("monthSum", monthSum.getMonthsum());
+      if(weekSum != null)
+         model.addAttribute("weekSum", weekSum.getWeeksum());
+      
+      
+      return "schedule/MyReport.tiles";
+      //schedule/MyReport.tiles
+   }
 }
